69ff63caf6a3c28fbdd6d2d99ef266008874ea99
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-07-13 18:06:59 -0500

    Fix some import errors

--- autoROIFuncs.py
+++ autoROIFuncs.py
@@ -4,14 +4,17 @@ Created on Fri Jul 13 11:48:23 2018
 
 @author: Nick
 """
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import backend
 
 def meanIOU(y_true, y_pred):
     prec = []
     for t in np.arange(0.5, 1.0, 0.05):
         y_pred_ = tf.to_int32(y_pred > t)
         score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-        K.get_session().run(tf.local_variables_initializer())
+        backend.get_session().run(tf.local_variables_initializer())
         with tf.control_dependencies([up_opt]):
             score = tf.identity(score)
         prec.append(score)
-    return K.mean(K.stack(prec), axis=0)
+    return backend.mean(backend.stack(prec), axis=0)
